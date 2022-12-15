8e65ff777e0f33c0d309270594c00181d6d16962
Junda Ai <39738304+Alan052918@users.noreply.github.com>
2022-12-15 16:58:24 -0500

    Add ADXL345 output reading using dependency

--- src/main.cpp
+++ src/main.cpp
@@ -161,8 +161,28 @@ void read_data_github()
     // printf("Raw|\tgx: %4.5f\tgy: %4.5f\tgz: %4.5f\n", data[0], data[1], data[2]);
 }
 
+ADXL345 adxl(PB_9, PB_8);
+
 void read_data_adxllib()
 {
+    adxl.setDataRate(0x0B);
+    adxl.setPowerMode(NORMAL_OPERATION);
+    adxl.setPowerControl(0b00'0'0'1'0'00);
+    adxl.setDataFormatControl(0b0'0'0'0'1'0'00);
+
+    while (1) {
+        int devid = adxl.getDevId();
+        if (devid != 0xE5) {
+            printf("ERROR: DEVID is not 0xE5\n");
+            return;
+        }
+
+        int readings[3];
+        adxl.getOutput(readings);
+        printf("x: %5d\ty: %5d\tz: %5d\n", readings[0], readings[1], readings[2]);
+
+        thread_sleep_for(100);
+    }
 }
 
 int main()
